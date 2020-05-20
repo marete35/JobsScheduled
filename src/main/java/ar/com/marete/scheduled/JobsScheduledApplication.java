f30cb72923e3d2c6aeff4e442741db6ad097519b
Mario <mgalarza@learsoft.com.ar>
2020-05-20 14:54:57 -0300

    Se agrego para mostrar info del pool de conexiones

--- src/main/java/ar/com/marete/scheduled/JobsScheduledApplication.java
+++ src/main/java/ar/com/marete/scheduled/JobsScheduledApplication.java
@@ -1,16 +1,30 @@
 package ar.com.marete.scheduled;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 @EnableScheduling
-public class JobsScheduledApplication {
-
+public class JobsScheduledApplication implements CommandLineRunner{
+	
+	@Autowired
+	DataSource dataSource;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JobsScheduledApplication.class, args);
+	}
+	
+	public void run(String... args) throws Exception{
+		System.out.println("DATASOURCE = " + dataSource);
+		HikariDataSource newds = (HikariDataSource)dataSource;
+        System.out.println("DATASOURCE MAX POOL = " + newds.getMaximumPoolSize());
 	}
 	
 }
